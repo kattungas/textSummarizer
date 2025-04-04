f5db2848c521b3b5689b9494286add2b19c2b094
kattungas <mail.srinivaskattunga@gmail.com>
2025-04-03 22:24:30 -0500

    removed files

--- test_vosk.py
+++
@@ -1,9 +0,0 @@
-import vosk
-import os
-
-model_path = "model/en"
-if os.path.exists(model_path):
-    model = vosk.Model(model_path)
-    print("✅ Vosk Model Loaded Successfully!")
-else:
-    print("❌ Model path is incorrect. Please check.")
